649e558c8cc7a96699b0c6ce24f749536fcc1642
Stone Preston <stonepreston@protonmail.com>
2022-03-04 18:26:24 -0600

    Get basic MIDI output working

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,25 +1,38 @@
 #include "MainComponent.h"
 
 //==============================================================================
-MainComponent::MainComponent()
+MainComponent::MainComponent(): 
+    defaultMIDIOutput(juce::MidiOutput::openDevice(juce::MidiOutput::getDefaultDevice().identifier)),
+    startTime(juce::Time::getMillisecondCounterHiRes() * 0.001)
 {
-    setSize (1200, 400);
+    DBG("Initialized with default Midi Device: " + defaultMIDIOutput->getDeviceInfo().name);
+    addAndMakeVisible(testButton);
+    testButton.setButtonText("Test");
+    testButton.addListener(this);
+    setSize(DEVICE_WIDTH * PX_TO_MM, DEVICE_HEIGHT * PX_TO_MM);
 }
 
 //==============================================================================
 void MainComponent::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (16.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.setFont(juce::Font (16.0f));
+    g.setColour(juce::Colours::white);
+    g.drawText("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
 {
-    // This is called when the MainComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+    testButton.setBounds(LEFT_EDGE_PADDING * PX_TO_MM, TOP_EDGE_PADDING * PX_TO_MM, KEY_WIDTH * PX_TO_MM, KEY_HEIGHT * PX_TO_MM);
+}
+
+void MainComponent::buttonClicked(juce::Button* button)
+{
+    if (button == &testButton) {
+        auto message = juce::MidiMessage::controllerEvent(1, 1, 127);
+        message.setTimeStamp(juce::Time::getMillisecondCounterHiRes() * 0.001 - startTime);
+        defaultMIDIOutput->sendMessageNow(message);
+    }
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -5,25 +5,56 @@
 // have called `juce_generate_juce_header(<thisTarget>)` in your CMakeLists.txt,
 // you could `#include <JuceHeader.h>` here instead, to make all your module headers visible.
 #include <juce_gui_extra/juce_gui_extra.h>
+#include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_audio_devices/juce_audio_devices.h>
 
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent   : public juce::Component
+class MainComponent   : public juce::Component, public juce::Button::Listener
 {
 public:
     //==============================================================================
     MainComponent();
 
     //==============================================================================
-    void paint (juce::Graphics&) override;
+    void paint(juce::Graphics&) override;
     void resized() override;
+    void buttonClicked(juce::Button* button) override;
 
 private:
     //==============================================================================
     // Your private member variables go here...
+    const double PX_TO_MM = 4;
+    const double TOP_EDGE_PADDING = 10.4775;
+    const double LEFT_EDGE_PADDING = TOP_EDGE_PADDING;
+    const double RIGHT_EDGE_PADDING = TOP_EDGE_PADDING;
+    const double BOTTOM_EDGE_PADDING = TOP_EDGE_PADDING;
+    const double KEY_WIDTH = 14;
+    const double KEY_HEIGHT = KEY_WIDTH;
+    const double HORIZONTAL_KEY_SPACING = 5.05;
+    const double VERTICAL_KEY_SPACING = HORIZONTAL_KEY_SPACING;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+    const double HYPERPIXEL_WIDTH = 98;
+    const double HYPERPIXEL_HEIGHT = 58.2;
+    const double HYPERPIXEL_SCREEN_WIDTH = (HYPERPIXEL_WIDTH - 6);
+
+    const double JOYSTICK_TOP_EDGE_PADDING = VERTICAL_KEY_SPACING;
+    const double JOYSTICK_LEFT_EDGE_PADDING = 0 * PX_TO_MM;
+    const double JOYSTICK_RIGHT_EDGE_PADDING = HORIZONTAL_KEY_SPACING;
+    const double JOYSTICK_BOTTOM_EDGE_PADDING	= 0;
+    const double ENCODER_SPACING = 7;
+
+    const int NUM_COLS = 16;
+    const int NUM_ROWS = 5;
+    const double DEVICE_WIDTH = (NUM_COLS * KEY_WIDTH) + ((NUM_COLS - 1) * HORIZONTAL_KEY_SPACING)  + LEFT_EDGE_PADDING + RIGHT_EDGE_PADDING;
+    const double DEVICE_HEIGHT = (NUM_ROWS * KEY_HEIGHT) + ((NUM_ROWS - 1) * VERTICAL_KEY_SPACING) + TOP_EDGE_PADDING + BOTTOM_EDGE_PADDING;
+    
+    std::unique_ptr<juce::MidiOutput> defaultMIDIOutput;
+    double startTime;
+    juce::TextButton testButton;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
